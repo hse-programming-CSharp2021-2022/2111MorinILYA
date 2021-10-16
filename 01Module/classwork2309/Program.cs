9758c312606a2dd4283d8c54cfddab00eb36ccd2
ILKA777 <90830436+ILKA777@users.noreply.github.com>
2021-10-16 23:42:12 +0300

    Add files via upload

---
+++ 01Module/classwork2309/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace classwork2309
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string a = Console.ReadLine();
+
+            if (!int.TryParse(a, out int n))
+            {
+                Console.WriteLine("Incorrect input");
+                return;
+            }
+            int k = 1;
+            double s = 0;
+            for (int i = 0; i <= n; i++)
+            {
+
+                s += (k + 0.3) / (3 * k * k + 5);
+                k += 1;
+
+            }
+
+            Console.WriteLine(s);
+        }
+    }
+}
