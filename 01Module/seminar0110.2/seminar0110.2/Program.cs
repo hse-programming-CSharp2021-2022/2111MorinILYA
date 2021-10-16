dc6d5b774851b314de013b7eea9b9e0d4c999c8d
ILKA777 <90830436+ILKA777@users.noreply.github.com>
2021-10-16 23:52:42 +0300

    Add files via upload

---
+++ 01Module/seminar0110.2/seminar0110.2/Program.cs
@@ -0,0 +1,69 @@
+﻿using System;
+
+namespace seminar0110._2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string a = Console.ReadLine();
+            
+
+            if (!int.TryParse(a, out int n))
+            {
+                Console.WriteLine("Incorrect input");
+                return;
+            }
+            Random random = new Random();
+
+            int[][] b = new int[n][];
+            for (int i=0;i<b.Length;i++)
+            {
+                b[i] = new int[random.Next(5, 7 + 1)];
+
+
+            }
+
+            for (int i = 0; i < n; i++)
+                for (int j = 0; j < b[i].Length; j++)
+                    b[i][j] = random.Next(-10, 10 + 1);
+
+            Array.ForEach(b, el =>
+            {
+                 Array.ForEach(el, elem => Console.Write($"{elem} "));
+                 Console.WriteLine();
+
+
+            });
+
+
+         
+
+            
+            
+
+
+
+
+
+
+            Array.ForEach(b, el =>
+            {
+                Array.ForEach(el, elem => Console.Write($"{elem} "));
+                Console.WriteLine();
+
+
+            });
+
+
+
+
+
+
+
+
+
+
+        }
+    }
+}
