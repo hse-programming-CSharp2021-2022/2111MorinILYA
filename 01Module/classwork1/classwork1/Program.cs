2f995d7e01fdae4a85eb5a3484e68a5058e0df97
ILKA777 <90830436+ILKA777@users.noreply.github.com>
2021-10-16 23:23:36 +0300

    Add files via upload

---
+++ 01Module/classwork1/classwork1/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+
+namespace classwork1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string b = Console.ReadLine();
+
+
+
+            if (!int.TryParse(b, out int a))
+            {
+                Console.WriteLine("Incorrect input");
+                return;
+            }
+
+
+            if (a == 1 | a == 2 | a == 3)
+            {
+                Console.WriteLine("Неудовлетворительно");
+
+            }
+
+
+            if (a == 4 | a == 5)
+            {
+
+                Console.WriteLine("Удовлетворительно");
+            }
+
+
+            if (a == 6 | a == 7)
+            {
+
+
+                Console.WriteLine("Хорошо");
+
+            }
+
+
+
+            if (a == 8 | a == 9 | a == 10)
+            {
+
+
+                Console.WriteLine(" Отлично");
+
+
+            }
+        }
+    }
+}
