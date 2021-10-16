4aaa6a4ed8b34945aee7f6fc1fab24dab630012c
ILKA777 <90830436+ILKA777@users.noreply.github.com>
2021-10-16 22:45:48 +0300

    Add files via upload

---
+++ 01Module/dz1/Program.cs
@@ -0,0 +1,65 @@
+﻿using System;
+
+namespace dz1
+{
+    class Program
+    {
+        static void Main()
+        {
+            double G=0;
+            string x = Console.ReadLine();
+            string y = Console.ReadLine();
+
+            if (!int.TryParse(x, out int x1))
+            {
+                Console.WriteLine("Incorrect input");
+                return;
+            }
+
+            if (!int.TryParse(y, out int y1))
+
+            {
+                Console.WriteLine("Incorrect input");
+                return;
+            }
+
+
+            if (x1<y1&&x1>0)
+            {
+
+                G = x1 + Math.Sin(y1);
+
+            }
+
+            if (x1>y1&&x1<0)
+            {
+
+                G = y1 - Math.Cos(x1);
+
+            }
+
+            else
+            {
+
+                G = 0.5 * x1 * y1;
+
+            }
+
+
+
+            Console.WriteLine(G);
+
+
+
+
+
+
+
+
+
+
+
+
+        }
+    }
+}
