1c163fd3ff949de652b96118e40b69bc295257f2
Илья Морин <ilka@MacBook-Pro-Ila.local>
2021-10-16 22:00:28 +0300

    some message

---
+++ 01Module/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace dz3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double g=0;
+            double x = Console.Read();
+
+
+            if (x<=0.5)
+            {
+                g = Math.Sin(90 * Math.PI / 180);
+            }
+
+
+            if (x > 0.5)
+            {
+                g = Math.Sin(90 * (Math.PI / 180)*(x-1));
+            }
+
+            Console.WriteLine(g);
+
+
+        }
+    }
+}
